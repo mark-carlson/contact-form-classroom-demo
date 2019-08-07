c06755171838d4941929cdb73d634982e775b49c
Mark Carlson <mark.carlson@takeoff.com>
2019-08-06 18:15:07 -0700

    Removes code for conducting an in-class live coding demo

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,180 +1,19 @@
-import React, { useState } from 'react';
-import { Button, Card, Col, Input, Preloader, Row, Icon } from 'react-materialize';
-import SweetAlert from 'sweetalert-react';
-import 'sweetalert/dist/sweetalert.css';
+import React from 'react';
+import { Row } from 'react-materialize';
+//import SweetAlert from 'sweetalert-react';
+//import 'sweetalert/dist/sweetalert.css';
 import './Contact.css';
 
 const Contact = () => {
-    const clearForm = {
-        fromEmail: '',
-        fromName: '',
-        subject: '',
-        body: '',
-        copy: false
-    };
-
-    const [userInput, setUserInput] = useState(clearForm)
-    const [emailValid, setEmailValid] = useState(false);
-    const [sendSuccessful, setSendSuccessful] = useState(false);
-    const [sendFailure, setSendFailure] = useState(false);
-    const [isSending, setIsSending] = useState(false);
-
-    const {fromEmail, fromName, subject, body, copy} = userInput;
 
 
-    const isEmailValid = (email) => {
-        setEmailValid(email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i));
-    }
-
-    const sendForm = () => {
-        setIsSending(true);
-
-        fetch('api/send', {
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            },
-            method: "post",
-            body: JSON.stringify(userInput)
-        }).then(result => {
-            if (result.status === 503) {
-                setSendFailure(true);
-                setIsSending(false);
-                setSendSuccessful(false);
-            }
-            return result.json();
-        }).then(data => {
-            if (data.message === "mail sent") {
-                setSendSuccessful(true);
-                setIsSending(false);
-                setSendFailure(false);
-                setUserInput(clearForm);
-                setEmailValid(false);
-            }
-        })
-    }
-
-    const buttonContent = () => (isSending) 
-        ? 
-            <Preloader small /> 
-        : 
-            <div>
-                <Icon right>send</Icon>
-                <span>Send Email</span>
-            </div>
+    // const isEmailValid = (email) => {
+    //     return email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)
+    // }
 
     return (
         <Row>
-            <Col s={12} l={6} offset="l3">
-                <Card className="darken-1"
-                textClassName="grey-text"
-                title="Email Form"
-                >
-                    <Row>
-                        <Input
-                            value={fromEmail}
-                            name="fromEmail"
-                            type="email"
-                            s={12}
-                            label="Email Address"
-                            onChange={e => {
-                                setUserInput({
-                                    ...userInput,
-                                    fromEmail: e.target.value
-                                })
-                                isEmailValid(e.target.value);
-                            }}
-                            className={(fromEmail.length && !emailValid) ? 'invalid' : 'valid'}
-                            autoComplete="off"
-                        />
-                    </Row>
-                    <Row>
-                        <Input
-                        value={fromName}
-                        name="fromName"
-                        type="text"
-                        s={12}
-                        label="Your Name"
-                        onChange={e => setUserInput({
-                            ...userInput,
-                            fromName: e.target.value
-                        })}
-                        />
-                    </Row>
-                    <Row>
-                        <Input
-                            value={subject}
-                            name="subject"
-                            type="text"
-                            s={12}
-                            label="Subject"
-                            onChange={e => setUserInput({
-                                ...userInput,
-                                subject: e.target.value
-                            })}
-                        />
-                    </Row>
-                    <Row>
-                        <Input
-                            value={body}
-                            name="body"
-                            type="textarea"
-                            s={12}
-                            label="Your message"
-                            onChange={e => setUserInput({
-                                ...userInput,
-                                body: e.target.value
-                            })}
-                        />
-                    </Row>
-                    <Row>
-                        <Input
-                            checked={copy}
-                            name="copy"
-                            type="checkbox"
-                            label="Send a copy to you?"
-                            onChange={() => setUserInput({
-                                ...userInput,
-                                copy: !copy
-                            })}
-                            s={12}
-                        />
-                    </Row>
-                    <Row>
-                        <Button
-                            s={3}
-                            large
-                            wave="light"
-                            disabled={!fromEmail || !subject || !body || !emailValid || !fromName || isSending}
-                            onClick={sendForm}
-                        >
-                        {buttonContent()}
-                        </Button>
-                    </Row>
-                    <SweetAlert
-                        show={sendSuccessful}
-                        type="success"
-                        title="Email Sent"
-                        text="Your email has been sent.  Thank you!"
-                        onConfirm={() => {
-                            setSendSuccessful(false);
-                            setIsSending(false);
-                        }
-                    }
-                    />
-                    <SweetAlert
-                        show={sendFailure}
-                        type="error"
-                        title="Email Failed"
-                        text="Your email was not sent.  Please try again."
-                        onConfirm={() => {
-                            setSendFailure(false);
-                            setIsSending(false)
-                        }
-                        }
-                    />
-                </Card>
-            </Col>
+            
         </Row>
     );
 };
--- middlewares/gmail.js
+++ middlewares/gmail.js
@@ -1,13 +1,3 @@
-/**
- * Create a .env file that contains your email credentials:
-
- GMAIL_USER = **your_email_address**@gmail.com
- GMAIL_PASS = **secret_password_here**
-
- */
-
-
-
 require('dotenv').config();
 const nodemailer = require("nodemailer");
 const fs = require("fs");
@@ -20,61 +10,7 @@ const elementMap = {
 }
 
 const sendMailFromGmail = (req, res, next) => {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.GMAIL_USER,
-            pass: process.env.GMAIL_PASS
-        }
-    }); //NOTE:  Use .env variables here for your user & pass.  Keep your credentials out of your github repo.
-
-    let htmlTemplate = fs.readFileSync(path.resolve(__dirname, '../templates/email.html'), 'UTF-8');
-
-    Object.keys(elementMap).forEach(key => {
-        htmlTemplate = htmlTemplate.replace(new RegExp(`__${elementMap[key]}__`, 'g'), req.body[key]);
-    });
-
-    htmlTemplate = htmlTemplate.replace(new RegExp(`__SITE__`, 'g'), "mc.dev"); // <-- enter your website here
-
-    const sendTo = [process.env.GMAIL_USER]; // array of recipients.  Add your email address here.
-
-    const mailOptions = {
-        from: `${req.body.fromName} <${req.body.fromEmail}>`, // sender address
-        to: sendTo, // list of receivers
-        subject: req.body.subject, // Subject line
-        text: req.body.body, // plain text body
-        html: htmlTemplate //html body
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log('error', error);
-            res.status(503).json({
-                message: 'send failed'
-            });
-        } else {
-            console.log('Email sent: ', info.response);
-            if (req.body.copy) {
-                mailOptions.to = `${req.body.fromName} <${req.body.fromEmail}>`;
-                transporter.sendMail(mailOptions, (error, info) => {
-                    if (error) {
-                        console.log('error', error);
-                        res.status(503).json({
-                            message: 'send failed'
-                        });
-                    } else {
-                        res.status(200).json({
-                            message: "mail sent"
-                        })
-                    }
-                })
-            } else {
-            res.status(200).json({
-                message: "mail sent"
-            })
-        }
-        }
-    })
+    
 }
 
 module.exports = sendMailFromGmail;
